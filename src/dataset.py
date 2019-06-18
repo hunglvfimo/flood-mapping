6aece48cec847ac07500904577d507d56f37e20e
Hung Luu <hunglv@fimo.edu.vn>
2019-06-18 23:07:40 +0900

    fix

--- src/dataset.py
+++ src/dataset.py
@@ -64,8 +64,7 @@ class SEMDataset(Dataset):
         return image, mask
 
     def __getitem__(self, index):
-        image = tiff.imread(self.image_paths[index], planarconfig='contig')
-
+        image = tiff.imread(self.image_paths[index])
         mask = Image.open(self.mask_paths[index]).convert('L')
         
         x, y = self.transform(image, mask)
